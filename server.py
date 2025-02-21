cb8a90a9121727cc3ef55cffff39253d3c6626a8
milkov <milkov.de@skbkontur.ru>
2025-02-21 20:44:43 +0500

    fix: import CORSMiddleware

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 from scipy.optimize import curve_fit
